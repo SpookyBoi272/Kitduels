6835f44c55d630d1e830c04cfbb497cd1ffb659d
Spooky Boi <74552206+NiranjanKattel-coder@users.noreply.github.com>
2023-09-09 21:12:20 +0545

    1st commit

--- src/main/java/prod/spooky/kitduels/Kitduels.java
+++ src/main/java/prod/spooky/kitduels/Kitduels.java
@@ -4,14 +4,12 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import prod.spooky.kitduels.commands.DuelCommand;
 
-import java.sql.SQLOutput;
-
 public final class Kitduels extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        System.out.println("Spooky Duels Plugin Sussessfully Loaded");
+        System.out.println("Spooky Duels Plugin Successfully Loaded");
         getCommand("Duel").setExecutor(new DuelCommand());
 
     }
--- src/main/java/prod/spooky/kitduels/utils/Duel.java
+++ src/main/java/prod/spooky/kitduels/utils/Duel.java
@@ -1,9 +1,12 @@
 package prod.spooky.kitduels.utils;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 public class Duel {
     public Duel(Player player, Player target){
+        player.sendMessage(ChatColor.RED+"[KitDuels] "+ChatColor.WHITE+"You have started Duel with" + target.getPlayer());
+        target.sendMessage(ChatColor.RED+"[KitDuels] "+ChatColor.WHITE+"You have started Duel with" + player.getPlayer());
 
     }
 }
