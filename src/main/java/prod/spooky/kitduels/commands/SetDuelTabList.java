bc37220e417700d7f28668603eb8c5237e78e453
Spooky Boi <74552206+NiranjanKattel-coder@users.noreply.github.com>
2023-11-13 22:29:04 +0545

    added set config

--- src/main/java/prod/spooky/kitduels/commands/SetDuelTabList.java
+++ src/main/java/prod/spooky/kitduels/commands/SetDuelTabList.java
@@ -12,17 +12,20 @@ import java.util.List;
 public class SetDuelTabList implements TabCompleter {
     public ArrayList<String> arena = new ArrayList<>();
     public ArrayList<String> spawnSide = new ArrayList<>();
+
+    public SetDuelTabList(){
+        arena.add("Arena1");
+        arena.add("Arena2");
+        arena.add("Arena3");
+        arena.add("Arena4");
+        spawnSide.add("spawn1");
+        spawnSide.add("spawn2");
+    }
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
         if (strings.length==1) {
-            arena.add("Arena1");
-            arena.add("Arena2");
-            arena.add("Arena3");
-            arena.add("Arena4");
             return arena;
         }else if (strings.length==2){
-            spawnSide.add("spawn1");
-            spawnSide.add("spawn2");
             return spawnSide;
         }
         return null;
